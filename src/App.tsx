b85eb418d8266bf2ebf450cd259e41218c2a7414
SarveshLimaye <sarveshlimaye2002@gmail.com>
2023-10-21 21:37:22 +0530

    initialized react-app + route-setup

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import UserRegistration from "./pages/User-Registration/UserRegistration";
+import AllMusic from "./pages/All-Music/AllMusic";
+import AddMusic from "./pages/Add-Music/AddMusic";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/all" element={<AllMusic />} />
+        <Route path="/user-registration" element={<UserRegistration />} />
+        <Route path="/add" element={<AddMusic />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
